0d52b94d8e6777e3c25c22bb904229f1a9e98117
piyush rathore <piysuh260305@gmail.com>
2024-10-29 22:58:34 +0530

    solve one more medium question

---
+++ 01-js/medium/times.js
@@ -0,0 +1,24 @@
+/*
+Write a function that calculates the time (in seconds) it takes for the JS code to calculate sum from 1 to n, given n as the input.
+Try running it for
+1. Sum from 1-100
+2. Sum from 1-100000
+3. Sum from 1-1000000000
+Hint - use Date class exposed in JS
+There is no automated test for this one, this is more for you to understand time goes up as computation goes up
+*/
+
+function calculateTime(n) {
+    let preTime = performance.now();
+    for (let i = 1; i <= n; i++) {
+        console.log(i);
+    }
+    let afterTime = performance.now();
+    let time = afterTime - preTime;
+    const timeInSec = time /1000;
+    console.log(timeInSec);
+    
+    return timeInSec
+}
+
+calculateTime(100000);
