a4f5cc552d60f54070ce3bd53c6f57d9257f5161
piyush rathore <piysuh260305@gmail.com>
2024-11-17 23:51:00 +0530

    assignments week 7 part 1 done

--- week-7-mongo-main/db.js
+++ week-7-mongo-main/db.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
+mongoose.connect("mongodb+srv://01piyush008:D85HDC3IOlfZgSHU@nodebackend.snfot.mongodb.net/todo_db")
+
 const User = new Schema({
   name: String,
   email: {type: String, unique: true},
@@ -12,7 +14,7 @@ const User = new Schema({
 const Todo = new Schema({
     userId: ObjectId,
     title: String,
-    done: Boolean
+   isCompleted: Boolean
 });
 
 const UserModel = mongoose.model('users', User);
