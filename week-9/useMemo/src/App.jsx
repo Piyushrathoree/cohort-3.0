7f26ea22415bf498c641ee95821762c3099bfa82
Piyushrathoree <meonpath008@gmail.com>
2024-12-31 17:08:03 +0530

    new codefiles added

--- week-9/useMemo/src/App.jsx
+++ week-9/useMemo/src/App.jsx
@@ -1,20 +1,31 @@
-import { useState ,useEffect} from "react";
+    import { useState ,useEffect, useMemo} from "react";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [counter, setCounter] = useState(0);
     const [num, setNum] = useState(0);
-    const [sum, setSum] = useState(0)
+    // const [sum, setSum] = useState(0)
 
-    function sumOfNum(num) {
+    // function sumOfNum(num) {
+    //     let sum = 0;
+    //     for (let i = 1; i <= num; i++) {
+    //         sum += i;
+    //     }
+    //     return sum;
+    // }
+    // useEffect(() => {
+    //     setSum(sumOfNum(num))
+    // })
+    //here i can also use useMemo hook for performance optimization and remove the extra re render 
+    // all i need is to save a loop in the usemem hook with a variable and the i can use the variable it self instead of the function and the third usestate variable
+
+    let count =useMemo(()=>{
         let sum = 0;
         for (let i = 1; i <= num; i++) {
             sum += i;
         }
         return sum;
-    }
-    useEffect(() => {
-        setSum(sumOfNum(num))
-    })
+    },[num])
+   
     return (
         <>
             <div className="flex flex-col justify-center items-center h-screen w-screen gap-5">
@@ -26,11 +37,11 @@ function App() {
                 />
 
                 <div>
-                    the sum of the number from 1 to inputed value is :{sum}
+                    the sum of the number from 1 to inputed value is :{count}
                     
                 </div>
-                <button onClick={() => setCount(count + 1)}>
-                    Increment {count}
+                <button onClick={() => setCounter(counter + 1)}>
+                    Increment {counter}
                 </button>
             </div>
         </>
