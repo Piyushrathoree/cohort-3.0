d9afd9f099cf722f0caec54ca3f85b6bd10dd9f5
Piyushrathoree <meonpath008@gmail.com>
2025-01-11 00:31:54 +0530

    added new project and revised react routing using react router dom

---
+++ week-11-reactYT/react-routing/routingProject/src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div className='h-screen w-screen flex items-center justify-center'>
+        <h1 className='text-bold text-3xl '>Page not Found</h1>
+    </div>
+  )
+}
+
+export default NotFound
