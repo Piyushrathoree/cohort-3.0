068dfbb38f3f2d1fba7e798cf9d8b01e87c95b05
Piyushrathoree <meonpath008@gmail.com>
2025-03-22 03:27:46 +0530

    add initial project setup with Tailwind CSS, utility functions, and UI components

--- week-11-reactYT/context-API/todoLocalContext/src/App.jsx
+++ week-11-reactYT/context-API/todoLocalContext/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Input } from "@/components/ui/input";
+import { Button } from "./components/ui/button";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [value, setValue] = React.useState("todo");
+    return (
+        <>
+            <div className="bg-primary  min-h-screen  flex justify-center items-center gap-3">
+                <Input className={'w-96 text-white border-transparent border border-gray-500 focus:shadow-md focus:shadow-gray-500'} placeholder={"Enter the todo"}/>  <Button className={"bg-[#ffffffbe]   hover:bg-transparent hover:border-gray-500 hover:shadow-md hover:shadow-gray-500  hover:border text-black  hover:text-white"} > Add  </Button>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
