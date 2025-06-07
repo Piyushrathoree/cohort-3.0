dcf6c5057625265bc69b52422c72773ef05a93cd
Piyushrathoree <meonpath008@gmail.com>
2025-06-08 00:56:33 +0530

    update index.js greeting message and add bcryptjs dependency in package-lock.json

--- week-26-devops-docker/index.js
+++ week-26-devops-docker/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World , I&aposm doing this using docker ');
+    res.send('Hello World , I&aposm doing this using docker  , here i made my first change');
 });
 
 app.listen(3000, () => {
